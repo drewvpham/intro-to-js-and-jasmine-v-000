eae09db5838effa9194619586415c7e0f7af5c06
Drew Pham <drewvpham@gmail.com>
2016-06-30 15:42:05 +0000

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function favIceCream(type){
+
+  return "I love "+type
+};
+
+function shouting(string){
+return string.toUpperCase();
+};
+
+function roundDown(num){
+  return Math.floor(num);
+};
+
+
+function theTruth(){
+
+  return true
+};
